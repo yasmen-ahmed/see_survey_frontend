d21b63d0390c97da1ff28ae28d21f0af901b78d4
Yasmeen Ahmed Mohamed (EXT-Nokia) <yasmeen.ahmed_mohamed.ext@nokia.com>
2025-08-03 16:39:38 +0300

    edit frontend

--- src/Components/LandingPageComponents/SurveyDashboard.jsx
+++ src/Components/LandingPageComponents/SurveyDashboard.jsx
@@ -29,9 +29,9 @@ const SurveyDashboard = ({ surveys, userRole }) => {
     <div className="mb-6">
     
       
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-7 gap-4 mb-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4 mb-6">
         {/* Total Projects Card */}
-        <div className="bg-white rounded-lg shadow-md p-2 border-l-4 border-blue-500">
+        {/* <div className="bg-white rounded-lg shadow-md p-2 border-l-4 border-blue-500">
           <div className="flex items-center">
             <div className="p-2 bg-blue-100 rounded-full">
               <svg className="w-6 h-6 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -43,7 +43,7 @@ const SurveyDashboard = ({ surveys, userRole }) => {
               <p className="text-2xl font-bold text-gray-900">{totalProjects}</p>
             </div>
           </div>
-        </div>
+        </div> */}
 
         {/* Total Surveys Card */}
         <div className="bg-white rounded-lg shadow-md p-2 border-l-4 border-blue-500">
@@ -96,16 +96,16 @@ const SurveyDashboard = ({ surveys, userRole }) => {
               </svg>
             </div>      
             <div className="ml-4">
-              <p className="text-sm font-medium text-gray-600">Under Review</p>
+              <p className="text-sm font-medium text-gray-600">Under Revision</p>
               <p className="text-2xl font-bold text-gray-900">{statusCounts.review || 0}</p>
             </div>
           </div>
         </div>
         {/* rework Card */}     
-        <div className="bg-white rounded-lg shadow-md p-2 border-l-4 border-blue-500">
+        <div className="bg-white rounded-lg shadow-md p-2 border-l-4 border-red-500">
           <div className="flex items-center">
-            <div className="p-2 bg-blue-100 rounded-full">
-              <svg className="w-6 h-6 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <div className="p-2 bg-red-100 rounded-full">
+              <svg className="w-6 h-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
               </svg>
                 </div>    
@@ -121,13 +121,13 @@ const SurveyDashboard = ({ surveys, userRole }) => {
             {/* Completed Surveys Card */}
         <div className="bg-white rounded-lg shadow-md p-2 border-l-4 border-green-500">
           <div className="flex items-center">
-            <div className="p-2 bg-indigo-100 rounded-full">
+            <div className="p-2 bg-green-100 rounded-full">
               <svg className="w-6 h-6 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
               </svg>
             </div>
             <div className="ml-4">
-              <p className="text-sm font-medium text-gray-600">Completed</p>
+              <p className="text-sm font-medium text-gray-600">Approved</p>
               <p className="text-2xl font-bold text-gray-900">{statusCounts.done || 0}</p>
             </div>
           </div>
